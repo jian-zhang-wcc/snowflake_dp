aee279df5e87b2c0a9d3393be0877ab47d1cf7c3
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:58 +1300

    chore: generated dbt artefacts from run 7b77bb9c

--- models/dataset/software_chargeback/dsschg_office365_usage.sql
+++ models/dataset/software_chargeback/dsschg_office365_usage.sql
@@ -1,3 +1,1 @@
-{{ gen_dataset('software_chargeback','office365_usage') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
-- -
-{{ gen_dataset_flat('software_chargeback','office365_usage') }}
+{{ gen_dataset('software_chargeback','office365_usage__flatten','office365_usage') }}
