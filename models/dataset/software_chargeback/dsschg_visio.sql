2764587d99c4c98091fcf67fea3cf9ee959013ba
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:42 +1300

    chore: generated dbt artefacts from run 7b77bb9c

---
+++ models/dataset/software_chargeback/dsschg_visio.sql
@@ -0,0 +1,3 @@
+{{ gen_dataset('software_chargeback','visio') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
+- -
+{{ gen_dataset_flat('software_chargeback','visio') }}
