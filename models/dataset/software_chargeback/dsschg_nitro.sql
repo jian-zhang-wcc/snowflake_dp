ef969c51d36a025bed9322f0a3d77281ed1353cd
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:29 +1300

    chore: generated dbt artefacts from run 7b77bb9c

---
+++ models/dataset/software_chargeback/dsschg_nitro.sql
@@ -0,0 +1,3 @@
+{{ gen_dataset('software_chargeback','nitro') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
+- -
+{{ gen_dataset_flat('software_chargeback','nitro') }}
