dd3dfc0ea26c5becf3949dc620fa8964d50806f0
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:41 +1300

    chore: generated dbt artefacts from run 7b77bb9c

---
+++ models/dataset/software_chargeback/dsschg_project_planner5.sql
@@ -0,0 +1,3 @@
+{{ gen_dataset('software_chargeback','project_planner5') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
+- -
+{{ gen_dataset_flat('software_chargeback','project_planner5') }}
