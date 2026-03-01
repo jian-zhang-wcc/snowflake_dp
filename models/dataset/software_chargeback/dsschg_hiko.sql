4deeef1bf84bfe8b00e0fc03e720ce8f89781d1a
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:49 +1300

    chore: generated dbt artefacts from run 7b77bb9c

--- models/dataset/software_chargeback/dsschg_hiko.sql
+++ models/dataset/software_chargeback/dsschg_hiko.sql
@@ -1,3 +1,1 @@
-{{ gen_dataset('software_chargeback','hiko') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
-- -
-{{ gen_dataset_flat('software_chargeback','hiko') }}
+{{ gen_dataset('software_chargeback','hiko__flatten','hiko') }}
