c6b647ecb718d1fd078d5dbb3e0257ce16c1d1aa
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:46 +1300

    chore: generated dbt artefacts from run 7b77bb9c

--- models/dataset/software_chargeback/dsschg_copilot_usage.sql
+++ models/dataset/software_chargeback/dsschg_copilot_usage.sql
@@ -1,3 +1,1 @@
-{{ gen_dataset('software_chargeback','copilot_usage') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
-- -
-{{ gen_dataset_flat('software_chargeback','copilot_usage') }}
+{{ gen_dataset('software_chargeback','copilot_usage__flatten','copilot_usage') }}
