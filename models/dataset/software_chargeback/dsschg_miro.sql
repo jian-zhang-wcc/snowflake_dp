1cf586deaacb50f3ea824a22dc359eaa712ada15
jian-zhang-wcc <jian.zhang@wcc.govt.nz>
2026-03-02 11:38:50 +1300

    chore: generated dbt artefacts from run 7b77bb9c

--- models/dataset/software_chargeback/dsschg_miro.sql
+++ models/dataset/software_chargeback/dsschg_miro.sql
@@ -1,3 +1,1 @@
-{{ gen_dataset('software_chargeback','miro') }}-- USE this is off flatten TODO Change meta_source_table to generate correct code 
-- -
-{{ gen_dataset_flat('software_chargeback','miro') }}
+{{ gen_dataset('software_chargeback','miro__flatten','miro') }}
